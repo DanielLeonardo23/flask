5efa520c280fb64f48c91934702c4dd4a3d02785
Daniel Leonardo Escribas Alan <daniel.escribas@unmsm.edu.pe>
2025-02-22 20:57:19 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,77 +1,41 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import os
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-import stripe
-
-# Definir las credenciales directamente en el código
-CLOUD_NAME = 'duruqbipv'
-API_KEY = '857167242619486'
-API_SECRET = 'POaaiNhqAICv8t91AXXD-ABx-D4'
-STRIPE_SECRET_KEY = 'sk_test_51Qol4rAkA9dBfeWxx5RjKhKauUxCpbR0gB5RwA21Cu0b316epbVrq9PUcvL0J5JqHfaAbKG0m3U1B5mVw5ngTatw00MAeQuK40'
-
-# Configuración de Cloudinary
-cloudinary.config(
-    cloud_name=CLOUD_NAME,
-    api_key=API_KEY,
-    api_secret=API_SECRET
-)
-
-# Configuración de Stripe
-stripe.api_key = STRIPE_SECRET_KEY
 
 app = Flask(__name__)
 
-# Lista para almacenar los links de las imágenes subidas
-uploaded_images = []
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Ruta principal que muestra el formulario para cargar imágenes
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Ruta para manejar la carga de imágenes
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
-        # Obtén la imagen de los datos recibidos a través de 'form-data'
-        if 'image' not in request.files:
-            return jsonify({'error': 'No se ha recibido una imagen'}), 400
+        print(request)
+        image_data = request.data
+
+    
+        if not image_data:
+            return jsonify({'error': 'No se han recibido datos'}), 400
+
         
-        image_file = request.files['image']
-
-        # Si no se ha recibido un archivo
-        if image_file.filename == '':
-            return jsonify({'error': 'No se ha seleccionado un archivo'}), 400
-
-        # Sube la imagen a Cloudinary
-        response = cloudinary.uploader.upload(image_file, 
-                                              resource_type="auto",  # Detecta automáticamente el tipo de archivo
-                                              folder="uploads")  # Carpeta en Cloudinary para almacenar las imágenes
-
-        # Obtén la URL de la imagen subida
-        image_url = response.get('secure_url')
-
-        # Almacenar el link de la imagen
-        uploaded_images.append(image_url)
-
-        return jsonify({'message': 'Imagen subida correctamente', 'image_url': image_url}), 200
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_image.jpg'), 'wb') as f:
+            f.write(image_data)
+        
+        return jsonify({'message': 'Imagen subida correctamente'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400
-
-# Ruta para ver las imágenes subidas
+    
 @app.route('/upload', methods=['GET'])
 def get_upload():
     try:
-        # Si hay imágenes subidas, devolverlas en formato JSON
-        if uploaded_images:
-            return jsonify({'image_urls': uploaded_images}), 200
-        else:
-            return jsonify({'message': 'No hay imágenes subidas.'}), 200
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_image.jpg'), 'rb') as f:
+            image_data = f.read()
+        
+        return image_data
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
+    # Obtener el puerto asignado por Render o usar 8000 por defecto
     port = int(os.environ.get('PORT', 8000))
+    # Asegurarse de que la aplicación escuche en todas las direcciones
     app.run(host='0.0.0.0', port=port, debug=True)
